480032304f2a91ee63d42ff9237dc12b951adc05
Paulius Matulevičius <besteris@hotmail.com>
2023-10-30 21:30:31 +0200

    Task5 added

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string preke = "Obuolys";
+            int kiekis = 5;
+            string kaina = "2 eur";
+
+            Console.WriteLine($"Preke: {preke}");
+            Console.WriteLine($"Kiekis: {kiekis}");
+            Console.WriteLine($"Kaina: {kaina}");
+
+            Console.ReadLine();
+        }
+    }
+}
