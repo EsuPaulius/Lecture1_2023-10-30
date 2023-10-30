d52f4114edaac5627e4791e7565db8257589ad8f
Paulius Matulevičius <besteris@hotmail.com>
2023-10-30 21:37:07 +0200

    Task6 added.

---
+++ Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = """
+                ==========VIZITINE==========
+                Vardas: Paulius
+                Amzius: 28
+                Pareigos: Developer
+                ============================
+                """;
+
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+        }
+    }
+}
